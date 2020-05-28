596e915ee6b34b1ff1c4b8580de5d2c4f8cee8a3
unknown <nicoletobe2000@gmail.com>
2020-05-28 22:05:07 +0400

    initial commit, just a mock

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const  HtmlWebpackPlugin =  require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+    entry:'./index.js',
+    optimization: {
+        minimize: false
+    },
+    output : {
+        path : path.resolve(__dirname , 'hubtree'),
+        filename: 'main.js'
+    },
+    plugins:  [
+        new HtmlWebpackPlugin ({
+            template : './public/popup.html'
+        }),
+        new CopyPlugin({
+            patterns: [
+              { from: 'public'}
+            ],
+          })
+    ]
+}
